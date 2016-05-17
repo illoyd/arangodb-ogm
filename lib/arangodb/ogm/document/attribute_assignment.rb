985c6b49f6f3cc163555b7275c0ed0d0d7dbc080
Ian Lloyd <ian.w.lloyd@me.com>
2016-05-17 16:54:56 +0100

    Attribute assignment shadowing protection
    Do not load an attribute key that shadows a function.

--- lib/arangodb/ogm/document/attribute_assignment.rb
+++ lib/arangodb/ogm/document/attribute_assignment.rb
@@ -9,9 +9,10 @@ module ArangoDB
 
         ##
         # Dynamically insert the attribute key into the attribute hash to
-        # implement the rest of the toolkit.
+        # implement the rest of the toolkit. Checks if the model can already respond
+        # to the key assignment so that we do not inadvertently shadow an existing method.
         def _assign_attribute(k, v)
-          @attributes[k] ||= nil
+          @attributes[k] ||= nil unless respond_to?("#{k}=")
           super
         end
 
